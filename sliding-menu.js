c1f6af79e42948e82a3b02d8ff45c5daa0b8cb1f
Daniel Santos <daniel.bess22@gmail.com>
2022-06-05 21:31:00 +0100

    commit

--- sliding-menu.js
+++ sliding-menu.js
@@ -8,6 +8,12 @@
                 <li>Test 3</li>
                 <li>Test 4</li>
             </ul>
+            <ul>
+                <li>Test 1</li>
+                <li>Test 2</li>
+                <li>Test 3</li>
+                <li>Test 4</li>
+            </ul>
         </nav>
     `;
 
