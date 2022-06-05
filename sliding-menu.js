06d30a4cdf6ae6c97c1e15d96532bdfeabfd6025
Daniel Santos <daniel.bess22@gmail.com>
2022-06-05 23:03:22 +0100

    commit

--- sliding-menu.js
+++ sliding-menu.js
@@ -71,6 +71,8 @@
 
         //When the custom widget is updated, the Custom Widget SDK framework executes this function after the update
 		onCustomWidgetAfterUpdate(oChangedProperties) {
+            var that = this;
+            loadthis(that);
             this.redraw();
         }
         
